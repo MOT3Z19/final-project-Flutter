e3cd6c31d6a56ac4198ae130fdeef9929dad9734
Mot3z <motazboss2001@gmail.com>
2022-05-20 03:03:43 +0300

    Final Project

---
+++ README.md
@@ -0,0 +1,21 @@
+# fruitmarket
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+
+![iPhone X, XS, 11 Pro – 28](https://user-images.githubusercontent.com/38792815/125530828-b2910371-d497-4a3b-ab68-e4ecd9282acf.png)
+![Favourite](https://user-images.githubusercontent.com/38792815/125530815-c414e5f5-63a8-4daa-9976-4d5c255f6ac8.png)
+![iPhone X, XS, 11 Pro – 27](https://user-images.githubusercontent.com/38792815/125530826-8400e057-4cd1-4527-b7ae-7f77d113fec6.png)
+![iPhone X, XS, 11 Pro – 5](https://user-images.githubusercontent.com/38792815/125531082-69f9c4d1-1545-4017-adc8-60cdba655b65.png)
+![Wishlist Page – 9](https://user-images.githubusercontent.com/38792815/125531094-850bf600-69ea-42ae-a80b-5fd638e29110.png)
+
+
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruitmarket/layout/market_Screen.dart';
+import 'package:fruitmarket/modules/introduction/splashscreen/Splash_Screen.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(MyApp());
+}
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print('onCreate -- ${bloc.runtimeType}');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('onChange -- ${bloc.runtimeType}, $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('onError -- ${bloc.runtimeType}, $error');
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    print('onClose -- ${bloc.runtimeType}');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: SplashScreen(),
+    );
+  }
+}
+//test
